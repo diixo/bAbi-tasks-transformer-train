96c92bfaee54deed71e14a288f6712bd9babe5b7
diixo <anatolii.podoba@gmail.com>
2025-09-18 18:38:42 +0300

    eval.py: start adaptation

--- eval.py
+++ eval.py
@@ -1,18 +1,23 @@
-from datasets import load_metric
+import evaluate
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from data import BabiqaDataset
+from data_slots import BabiQADatasetSlots
 from utils import parse_answer
-import sys
+import torch
 import pandas as pd
 import os
 
-tokenizer = AutoTokenizer.from_pretrained(sys.argv[-1])
-model = AutoModelForCausalLM.from_pretrained(sys.argv[-1], device_map="auto")
 
-for task_id in range(20):
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model_dir = "my-model"
+
+tokenizer = AutoTokenizer.from_pretrained(model_dir)
+model = AutoModelForCausalLM.from_pretrained(model_dir)
+model.to(device)
+
+for task_id in range(1):
     task_no = f"qa{task_id+1}"
-    test_dataset = BabiqaDataset(tokenizer, split="test", task_no=task_no)
-    test_dataset_raw = BabiqaDataset(
+    test_dataset = BabiQADatasetSlots(tokenizer, split="test", task_no=task_no)
+    test_dataset_raw = BabiQADatasetSlots(
         tokenizer, split="test", retrun_object=True, task_no=task_no
     )
     df = pd.DataFrame(
@@ -26,7 +31,7 @@ for task_id in range(20):
         raw_data = test_dataset_raw[data_idx]
         output_text = tokenizer.decode(
             model.generate(
-                data["input_ids"],
+                data["input_ids"].to(device),
                 max_new_tokens=30,
                 do_sample=False,
                 eos_token_id=tokenizer.eos_token_id,
@@ -69,7 +74,7 @@ for task_id in range(20):
         # if data_idx == 30:
             # break
 
-    metric = load_metric("accuracy")
+    metric = evaluate.load_metric("accuracy")
     accuracy = metric.compute(predictions=model_prediction, references=references)
     print(task_no, accuracy)
     acc = accuracy["accuracy"]
