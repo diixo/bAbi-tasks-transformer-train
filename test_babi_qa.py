d60d5fe6b253f068922b06038990b5e471032720
diixo <anatolii.podoba@gmail.com>
2025-09-20 01:02:12 +0300

    renamed: from babi_qa/hf_babi_qa.py to babi_qa/babi_qa_slots.py

--- test_babi_qa.py
+++ test_babi_qa.py
@@ -4,11 +4,11 @@ from datasets import load_dataset, load_dataset_builder
 
 if __name__ == "__main__":
 
-    dataset = load_dataset("babi_qa/hf_babi_qa.py", type="en", task_no = "qa1", trust_remote_code=True)
+    dataset = load_dataset("babi_qa/babi_qa_slots.py", type="en", task_no = "qa1", trust_remote_code=True)
     train_ds = dataset["train"]
     test_ds = dataset["test"]
 
-    builder = load_dataset_builder("babi_qa/hf_babi_qa.py", "en-qa1")
+    builder = load_dataset_builder("babi_qa/babi_qa_slots.py", "en-qa1")
 
     filepath = "babi_qa/test-items.txt"
     files = [(filepath, open(filepath, "rb"))]
