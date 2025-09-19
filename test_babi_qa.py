ac96d446c77938dc86972125d6619aad390718a5
diixo <anatolii.podoba@gmail.com>
2025-09-20 00:05:57 +0300

    test_babi_qa.py: impl

--- test_babi_qa.py
+++ test_babi_qa.py
@@ -1,8 +1,18 @@
-from babi_qa.hf_babi_qa import BabiQa, BabiQaConfig
+
+from datasets import load_dataset, load_dataset_builder
 
 
 if __name__ == "__main__":
 
-    builder = BabiQa(config=BabiQaConfig(type="shuffled-10k", task_no="qa1"))
-    dataset = builder.as_dataset()
-    print(dataset["train"][0])
+    dataset = load_dataset("babi_qa/hf_babi_qa.py", type="en", task_no = "qa1", trust_remote_code=True)
+    train_ds = dataset["train"]
+    test_ds = dataset["test"]
+
+    builder = load_dataset_builder("babi_qa/hf_babi_qa.py", "en-qa1")
+
+    filepath = "babi_qa/test-items.txt"
+    files = [(filepath, open(filepath, "rb"))]
+
+    for key, value in builder._generate_examples(filepath=filepath, files=files):
+        print("Key:", key)
+        print("Value:", value)
