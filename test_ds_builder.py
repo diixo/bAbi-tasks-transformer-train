ca0800accba385f9eff4bcbbbeaffc0bd705bca6
diixo <anatolii.podoba@gmail.com>
2025-09-20 06:42:40 +0300

    test_ds_builder.py: updated...

--- test_ds_builder.py
+++ test_ds_builder.py
@@ -4,18 +4,14 @@ import json
 from collections import defaultdict
 
 
-BABI_QA_SLOTS_DIR = "babi_qa_slots/"
+BABI_QA_SLOTS = "babi_qa_slots/babi_qa_slots.py"
 
 
 if __name__ == "__main__":
 
-    dataset = load_dataset(BABI_QA_SLOTS_DIR + "babi_qa_slots.py", type="en", task_no = "qa1", trust_remote_code=True)
-    train_ds = dataset["train"]
-    test_ds = dataset["test"]
+    builder = load_dataset_builder(BABI_QA_SLOTS, "en-qa1")
 
-    builder = load_dataset_builder(BABI_QA_SLOTS_DIR + "babi_qa_slots.py", "en-qa1")
-
-    filepath = BABI_QA_SLOTS_DIR + "test-items.txt"
+    filepath = "babi_qa_slots/test-items.txt"
     files = [(filepath, open(filepath, "rb"))]
 
     stories = []
@@ -33,10 +29,14 @@ if __name__ == "__main__":
         #print("---------------------------------------\nitem:", story)
         stories.append(story)
 
-    train_ds = builder.raw_to_json()["train"]
+    # need to unzip tasks archive
+    #train_ds = builder.raw_to_json()["train"]
 
-    with open(BABI_QA_SLOTS_DIR + "test-items.json", "w", encoding="utf-8") as f:
-        json.dump(train_ds, f, ensure_ascii=False, indent=2)
+    # with open("datasets/test-items.json", "w", encoding="utf-8") as f:
+    #     json.dump(train_ds, f, ensure_ascii=False, indent=2)
 
-    # dataset = load_dataset('babi_qa', type='en', task_no="qa1", trust_remote_code=True)
-    # print(len(dataset["train"]))
+    dataset = load_dataset('babi_qa', type='en', task_no="qa1", trust_remote_code=True)
+    print(len(dataset["train"]))
+
+    dataset = load_dataset(BABI_QA_SLOTS, type="en", task_no = "qa1", trust_remote_code=True)
+    print(len(dataset["train"]))
